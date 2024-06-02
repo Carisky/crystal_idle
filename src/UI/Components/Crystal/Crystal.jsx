9550360219208b1c00dbeb799eb4a7917ddec683
Yehor <egorkorsun7@gmail.com>
2024-06-02 22:13:23 +0200

    use states from reducer

--- src/UI/Components/Crystal/Crystal.jsx
+++ src/UI/Components/Crystal/Crystal.jsx
@@ -1,21 +1,29 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { incrementByAmount } from "../../../redux/reducer/reducer";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { incrementByAmount } from '../../../redux/reducer/reducer';
 
-export default function Crystal({crystalClass, coinsPerSec}) {
+export default function Crystal() {
   const dispatch = useDispatch();
+  const coinsPerSec = useSelector((state) => state.state.coinsPerSec);
+  const crystallClass = useSelector((state) => state.state.crystallClass);
+  const crystallLevel = useSelector((state) => state.state.crystallLevel);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      dispatch(incrementByAmount(coinsPerSec/20));
-    }, 50);
+    if (!isNaN(coinsPerSec)) {
+      const interval = setInterval(() => {
+        console.log(coinsPerSec * crystallLevel/ 20)
+        dispatch(incrementByAmount(coinsPerSec * crystallLevel/ 20));
+      }, 50);
 
-    return () => clearInterval(interval);
-  }, [dispatch]);
+      return () => clearInterval(interval);
+    }
+  }, [dispatch,coinsPerSec]);
 
   return (
-    <div>
-      <h1>{crystalClass}</h1>
+    <div style={{
+      
+    }}>
+      <h1>{crystallClass}</h1>
     </div>
   );
 }
